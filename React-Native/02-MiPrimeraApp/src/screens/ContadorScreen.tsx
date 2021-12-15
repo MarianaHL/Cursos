a7aa636d7e6cea6af6661ef79a0f96c1523b3e10
Mariana Hernandez <mariana.hl.1999@gmail.com>
2021-12-15 10:38:35 -0600

    Mi primera app en react native

---
+++ React-Native/02-MiPrimeraApp/src/screens/ContadorScreen.tsx
@@ -0,0 +1,41 @@
+import React, {useState} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import Fab from '../components/Fab';
+
+const ContadorScreen = () => {
+  const [contador, setContador] = useState(10);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Contador: {contador}</Text>
+
+      <Fab title="+1" onPress={() => setContador(contador + 1)} />
+
+      <Fab title="-1" position="bl" onPress={() => setContador(contador - 1)} />
+
+      {/* <TouchableOpacity
+                style={ styles.fabLocationBL }
+                onPress={ () => setContador( contador - 1 ) }
+            >
+                <View style={ styles.fab }>
+                    <Text style={ styles.fabText }> -1 </Text>
+                </View>
+            </TouchableOpacity>
+             */}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 40,
+    top: -15,
+    textAlign: 'center',
+  },
+});
+
+export default ContadorScreen;
